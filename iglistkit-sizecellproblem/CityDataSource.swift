4ee2789ae0e115bf5d8ddc7511e591135a09744e
Giuseppe Bottiglieri <giuseppe.bottiglieri@gmail.com>
2017-08-04 09:35:28 +0200

    first commit

---
+++ iglistkit-sizecellproblem/CityDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  CityDataSource.swift
+//  iglistkit-sizecellproblem
+//
+//  Created by Giuseppe Bottiglieri on 04/08/17.
+//  Copyright © 2017 GiuBot. All rights reserved.
+//
+
+import Foundation
