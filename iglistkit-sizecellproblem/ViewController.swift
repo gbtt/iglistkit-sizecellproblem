55c476199c0e66e7dc39f192604cf1b026ac7bd3
Giuseppe Bottiglieri <giuseppe.bottiglieri@gmail.com>
2017-08-04 08:21:07 +0200

    Initial Commit

---
+++ iglistkit-sizecellproblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iglistkit-sizecellproblem
+//
+//  Created by Giuseppe Bottiglieri on 04/08/17.
+//  Copyright © 2017 GiuBot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
